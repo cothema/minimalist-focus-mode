11540b0d1c9d86d432e11093376a7568afee3aec
Milos Havlicek <miloshavlicek@gmail.com>
2025-02-14 22:00:26 +0100

    feat: Prototype: Changing icon based on mode, added Analytics mode, added Dopamin Index

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,51 +1,72 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const toggleButton = document.getElementById("toggleMode");
-  const modePicker = document.getElementById("modePicker");
+  const modeButtons = document.querySelectorAll(".mode-button");
+  const settingsIcon = document.getElementById("settingsIcon");
+  const dopaminInfo = document.getElementById("dopaminInfo");
+  const dopaminIndex = document.getElementById("dopaminIndexValue");
 
-  chrome.storage.sync.get(["focusedMode", "selectedMode"], (data) => {
-    const isFocused = data.focusedMode || false;
-    const selectedMode = data.selectedMode || "create";
-    updateButtonState(isFocused);
-    modePicker.value = selectedMode;
+  chrome.storage.sync.get(["selectedMode"], (data) => {
+    const selectedMode = data.selectedMode || "disabled";
+    updateDopaminIndex(selectedMode);
+    highlightSelectedMode(selectedMode);
+    updateExtensionIcon(selectedMode);
   });
 
-  toggleButton.addEventListener("click", () => {
-    toggleFocusMode();
-  });
-
-  modePicker.addEventListener("change", () => {
-    chrome.storage.sync.get("focusedMode", (data) => {
-      if (!data.focusedMode) {
-        toggleFocusMode();
-      } else {
-        chrome.storage.sync.set({ selectedMode: modePicker.value }, () => {
-          chrome.runtime.sendMessage({ action: "reloadPage" });
-          window.close();
-        });
-      }
+  modeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const selectedMode = button.getAttribute("data-mode");
+      chrome.storage.sync.set({selectedMode: selectedMode}, () => {
+        updateDopaminIndex(selectedMode);
+        highlightSelectedMode(selectedMode);
+        updateExtensionIcon(selectedMode);
+        chrome.runtime.sendMessage({action: "reloadPage"});
+        window.close();
+      });
     });
   });
 
-  function toggleFocusMode() {
-    chrome.storage.sync.get("focusedMode", (data) => {
-      const newMode = !data.focusedMode;
-      const selectedMode = modePicker.value;
-      chrome.storage.sync.set({ focusedMode: newMode, selectedMode: selectedMode }, () => {
-        updateButtonState(newMode);
-        chrome.runtime.sendMessage({ action: "toggleMode", mode: selectedMode }, (response) => {
-          if (response && response.success) {
-            console.log("Mode toggled successfully");
-            chrome.runtime.sendMessage({ action: "reloadPage" });
-            window.close();
-          }
-        });
-      });
+  settingsIcon.addEventListener("click", () => {
+    chrome.tabs.create({url: "src/settings/settings.html"});
+  });
+
+  dopaminInfo.addEventListener("click", () => {
+    chrome.tabs.create({url: "src/settings/settings.html"});
+  });
+
+  function updateDopaminIndex(mode) {
+    let index = 0;
+    switch (mode) {
+      case "create":
+        index = 100;
+        break;
+      case "networking":
+        index = 30;
+        break;
+      case "analytics":
+        index = 20;
+        break;
+      case "inspiration":
+        index = 10;
+        break;
+      case "play":
+      case "disabled":
+        index = 0;
+        break;
+    }
+    dopaminIndex.textContent = `${index}%`;
+    dopaminIndex.style.color = `rgb(${255 - (index * 2.55)}, ${index * 2.55}, 0)`;
+  }
+
+  function highlightSelectedMode(mode) {
+    modeButtons.forEach(button => {
+      button.classList.toggle("enabled", button.getAttribute("data-mode") === mode);
+
+      const isDisabled = button.getAttribute("data-mode") === "disabled";
+      button.style.display = (mode === "disabled" && isDisabled) ? "none" : "block";
     });
   }
 
-  function updateButtonState(isFocused) {
-    console.log(`User request: ${isFocused ? "enable" : "disable"} focus mode`);
-    toggleButton.textContent = isFocused ? "Disable" : "Enable";
-    toggleButton.className = isFocused ? "enabled" : "disabled";
+  function updateExtensionIcon(mode) {
+    let iconPath = `../../icons/mode/${mode}.png`;
+    chrome.action.setIcon({path: iconPath});
   }
 });
