9dcd09bdd1159bcdf17214f24d3a7c570e8bd4f4
Milos Havlicek <miloshavlicek@gmail.com>
2025-02-15 10:02:09 +0100

    chore: Cleanup background.js code

--- src/background.js
+++ src/background.js
@@ -1,18 +1,19 @@
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.sync.get(["selectedMode"], (data) => {
+    const selectedMode = data.selectedMode || "disabled";
+    updateExtensionIcon(selectedMode);
+  });
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "toggleMode") {
-    chrome.storage.sync.get("focusedMode", (data) => {
-      const newMode = !data.focusedMode;
-      chrome.storage.sync.set({ focusedMode: newMode }, () => {
-        sendResponse({ success: true, focusedMode: newMode });
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          chrome.tabs.sendMessage(tabs[0].id, { action: "toggleMode", focused: newMode, mode: request.mode });
-        });
-      });
-    });
-    return true; // Keep the message channel open for sendResponse
-  } else if (request.action === "reloadPage") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  if (request.action === "reloadPage") {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
       chrome.tabs.reload(tabs[0].id);
     });
   }
 });
+
+function updateExtensionIcon(mode) {
+  let iconPath = `../../icons/mode/${mode}.png`;
+  chrome.action.setIcon({path: iconPath});
+}
