31689014ef6a102ff98391807ac00db3d07c3f93
Milos Havlicek <miloshavlicek@gmail.com>
2025-02-17 13:31:52 +0100

    feat: Translated strings in settings + new supported languages

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {createRoot} from "react-dom/client";
 import {DEFAULT_SETTINGS_MODES, Mode, SettingsModes, MODES} from "../lib/modes";
+import {t} from "../lib/i18n";
 
 const Settings: React.FC = () => {
     const [settingsModes, setSettingsModes] = useState<SettingsModes>(DEFAULT_SETTINGS_MODES);
@@ -29,11 +30,11 @@ const Settings: React.FC = () => {
         <>
             <div className="menu">
                 <div className="menu-item">
-                    About the Extension
+                   {t('settingsAbout')}
                 </div>
                 <a href="mailto:ceo+focusmode@cothema.com" target="_blank" rel="noopener noreferrer">
                     <div className="p-2 hover:bg-gray-700 cursor-pointer" id="report">
-                        Report Issue
+                      {t('reportIssue')}
                     </div>
                 </a>
             </div>
@@ -41,20 +42,17 @@ const Settings: React.FC = () => {
             <div className="content">
                 <div className="content-text">
                     <p>
-                        Minimalist Focus Mode is a Chrome extension designed to help users stay focused by hiding
-                        distracting
-                        elements.
-                        This extension allows users to toggle focus mode, which hides specific elements.
+                      {t('settingsDescription')}
                     </p>
 
-                    <h2>Supported websites</h2>
+                    <h2>{t('settingsSupportedWebsites')}</h2>
                     <ul>
                         <li>LinkedIn</li>
                         <li>Facebook</li>
                         <li>YouTube</li>
                     </ul>
 
-                    <h2>Enabled Focus Modes:</h2>
+                    <h2>{t('settingsEnabledModes')}:</h2>
                     {MODES.map((mode) => (
                         <div key={mode}>
                             <label>
@@ -63,7 +61,7 @@ const Settings: React.FC = () => {
                                     checked={settingsModes[mode]}
                                     onChange={() => handleToggle(mode)}
                                 />
-                                {mode.charAt(0).toUpperCase() + mode.slice(1)} Mode
+                                {t(`mode${mode.charAt(0).toUpperCase() + mode.slice(1)}`)}
                             </label>
                         </div>
                     ))}
@@ -71,13 +69,13 @@ const Settings: React.FC = () => {
 
                 <footer>
                     <p>
-                        The extension is licensed under Apache License Version 2.0.<br/>
-                        You can find the source code on <a href="https://github.com/cothema/minimalist-focus-mode/"
+                        {t('settingsLicense')}<br/>
+                        {t('settingsSourceCode')} <a href="https://github.com/cothema/minimalist-focus-mode/"
                                                            target="_blank"
                                                            rel="noopener noreferrer">GitHub</a>.
                     </p>
                     <p>
-                        Created by <a href="https://cothema.com/" target="_blank" rel="noopener noreferrer">
+                      {t('settingsCreatedBy')} <a href="https://cothema.com/" target="_blank" rel="noopener noreferrer">
                         Cothema s.r.o.</a>
                     </p>
                 </footer>
